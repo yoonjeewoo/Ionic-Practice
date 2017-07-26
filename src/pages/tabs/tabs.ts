bc235886a2d42876d5e0e1459402fcb2f33b9718
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-07-26 13:17:26 +0900

    탭 추가

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 // import { AboutPage } from '../about/about';
 import { HomePage } from '../home/home';
 import {PlaygroundPage} from "../playground/playground";
+import {RegisterPage} from "../register/register";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,7 +12,7 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = PlaygroundPage;
-
+  tab3Root = RegisterPage;
   constructor() {
 
   }
